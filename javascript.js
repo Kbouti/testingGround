78e434abdbe782db743a6f7f75ddcd6f69ea657f
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-06 16:30:19 -0700

    Add jim and el evil Jeffe example

---
+++ javascript.js
@@ -0,0 +1,25 @@
+const Player = (name, level) => {
+    let health = level * 2;
+    const getLevel = () => level;
+    const getName = () =>name;
+    const die = () => {
+
+    }
+    const damage = x => {
+        health -=x;
+        if (health <= 0){
+            die();
+        }
+    }
+    const attack = enemy =>{
+        if (level < enemy.getLevel()){
+            damaage(1);
+            console.log(`${name} has damaged ${enemy.getName()}`);
+        }
+    };
+    return {attack, damage, getLevel, getName};
+};
+
+const jimmie = Player(`Jim`, 10);
+const badGuy = Player(`Evil Jeffe`, 5);
+jimmie.attack(badGuy);
