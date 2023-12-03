3cede4e1f8031cac98ba62a40449872c8531d66e
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-12-02 19:42:55 -0800

    Complete recursion exercises

--- javascript.js
+++ javascript.js
@@ -334,38 +334,35 @@ console.log(sumSquares(testArray4));
 // ****************************************************************************************************************************
 // 9. The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
 
-function replicate(times, number){
-  if (times <= 0){
-    console.log(`times value is less than or equal to zero, returning zero`)
+function replicate(times, number) {
+  if (times <= 0) {
+    console.log("times value is less than or equal to zero, returning zero");
     return 0;
-  } else {
-    let array = [];
-    for (let i = 0; i < times;i++){
-array.push(number);
-    }
-    return array
   }
+  const array = [];
+  for (let i = 0; i < times; i++) {
+    array.push(number);
+  }
+  return array;
 }
 // This doesn't actually use recursion, but it solves the problem...
 
-// console.log(replicate(3,5))
-// console.log(replicate(11,5))
-
-function replicateRecursion(times, number){
+function myReplicate(times, number){
   let array = [];
   if (times < 1){
     return array;
   }
-  console.log(`array = ${array}`)
   array.push(number);
-  console.log(`array now = ${array}`)
-  
-  console.log(`times = ${times}`);
   times -= 1;
-  console.log(`times now = ${times}`);
-  
-  replicateRecursion(times, number)
+  array = array.concat(myReplicate(times, number))
   return array;
 }
+// BOOM! I struggled on this because I couldn't think to concat the arrays. I was creating a new array each time and returning it, concat adds the new array to the old one. 
+console.log(myReplicate(3, 5));
+console.log(myReplicate(12, 575));
 
-console.log(replicateRecursion(3, 5))
+// Here's their solutioin: 
+function theirReplicate(times, number){
+	if(times <= 0) return [];
+	return [number].concat(replicate(times - 1, number));
+}
