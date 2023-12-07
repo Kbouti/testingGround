70d7e958dfda3a95cf882fd5389bbad54580f07b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-12-07 13:33:02 -0800

    Work on creating separate merge function

--- javascript.js
+++ javascript.js
@@ -1,64 +1,62 @@
 // ******************************************************************************************************************
 // Merge Sort:
 // Build a function mergeSort that takes in an array and returns a sorted array, using recursive merge sort methodology.
-console.log("************************");
 function mergeSort(array) {
   const arrayLength = array.length;
-  if (array.length === 1) return array[0];
+  if (arrayLength === 1) return array[0];
   if (arrayLength > 1) {
     let arrayHalf;
     if (arrayLength % 2 !== 0) {
-      arrayHalf = arrayLength / 2 - 0.5;
+      arrayHalf = arrayLength / 2 + 0.5;
       console.log(`odd length array half: ${arrayHalf}`);
     } else {
       arrayHalf = arrayLength / 2;
       console.log(`even length array half: ${arrayHalf}`);
     }
 
-    const arrayA = [];
-    arrayA.push(array.slice(0, arrayHalf));
+    let arrayA = [];
+    arrayA = array.slice(0, arrayHalf);
+    console.log(arrayA);
 
-    const arrayB = [];
-    arrayB.push(array.slice(arrayHalf));
+    let arrayB = [];
+    arrayB = array.slice(arrayHalf);
+    console.log(arrayB);
 
     console.log(`arrayA = ${arrayA}`);
     console.log(`arrayB = ${arrayB}`);
 
-    const newA = mergeSort(arrayA);
-    const newB = mergeSort(arrayB);
+    console.log(arrayA.length);
 
-    // now we have to merge these two arrays.
-
-    const arrayC = [];
-    console.log(`Established arrayC: ${arrayC}`);
-
-    for (let a = 0; a < newA.length; a++) {
-      console.log("for loop initiated");
-      console.log(`comparing ${newA[a]} < ${newB[0]}`);
-
-      // Ok, the problem is that once it's gone through arrayA, it keeps trying to compare to the next index of arrayA which is undefined, so it just adds the next from B, and the next, and the next. Without sorting the remaining B's.
-      // Also, 5 is getting left out because it loses it's comparison round, then doesn't get checked again.
-      // We've successfully split the array in two. Our issue is with the merging function Nooooo, we gotta sort both of these first. Then merge. 
-
-      if (newA[a] < newB[0]) {
-        console.log(`element in array A is lower, adding ${newA[a]} to arrayC`);
-        arrayC.push(newA[a]);
-        console.log(`arrayC: ${arrayC}`);
-      } else {
-        console.log(`Element in B is lower, adding ${newB[0]} to arrayC`);
-        arrayC.push(newB.shift());
-
-        newB.push(newA[a])
-        console.log(`arrayC: ${arrayC}`);
+    if (arrayA.length > 2) {
+      console.log("arrayA length is greater than 2, calling mergeSort again");
+      arrayA = mergeSort(arrayA);
+      arrayB = mergeSort(arrayB);
+    }
+    if (arrayA.length === 2) {
+      if (arrayA[0] < arrayA[1]) {
+        return arrayA;
+      }
+      if (arrayA[0] > arrayA[1]) {
+        arrayA = [arrayA[1], arrayA[0]];
+        console.log(`arrayA: ${arrayA}`);
+      }
+    }
+    if (arrayB.length === 2) {
+      if (arrayB[0] < arrayB[1]) {
+        return arrayB;
+      }
+      if (arrayB[0] > arrayB[1]) {
+        arrayB = [arrayB[1], arrayB[0]];
+        console.log(`arrayB: ${arrayB}`);
       }
     }
 
+    console.log("We should have two 2-long sorted arrays now");
 
-    console.log(newA);
-    console.log(newB);
-    console.log(arrayC);
+    // const newA = mergeSort(arrayA);
+    // const newB = mergeSort(arrayB);
 
-    return arrayC;
+    // now we have to merge these two arrays.
   }
 }
 
@@ -69,5 +67,48 @@ function mergeSort(array) {
 // Return that new array
 
 const mergeTestArray = [5, 2, 3, 1, 6, 4, 7];
-const mergeTestArray2 = [5, 2];
-console.log(mergeSort(mergeTestArray));
+const mergeTestArray2 = [8, 9];
+console.log(mergeSort(mergeTestArray2));
+
+function merge(array1, array2) {
+  const array3 = [];
+  console.log(`array1 length: ${array1.length}`);
+  if (array1.length > array2.length) {
+    console.log(`array1 is longer`);
+    console.log(array1[0])
+    console.log(array2[0])
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[0] < array2[0]) {
+        console.log(
+          `array1: ${array1}  --array2: ${array2}  --array3: ${array3}`,
+        );
+        console.log(
+          `element in array1 is lower. removing ${array1[0]} and pushing it to array3`,
+        );
+
+        array3.push(array1[0]);
+        array1.splice(1, 0);
+
+        console.log(
+          `array1: ${array1}  --array2: ${array2}  --array3: ${array3}`,
+        );
+      } else if (array1[0] > array2[0]) {
+        console.log(
+          `array1: ${array1}  --array2: ${array2}  --array3: ${array3}`,
+        );
+        console.log(
+          `element in array2 is lower. removing ${array2[0]} and pushing it to array3`,
+        );
+
+        array3.push(array2[0]);
+        array2.splice(1, 0);
+
+        console.log(
+          `array1: ${array1}  --array2: ${array2}  --array3: ${array3}`,
+        );
+      }
+    }
+  }
+}
+
+merge(mergeTestArray, mergeTestArray2);
