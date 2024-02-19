d677d683a1c4307600548dbd1c08dbd06587e1ba
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-02-19 14:14:30 -0800

    Add opacity fade in example

--- javascript.js
+++ javascript.js
@@ -1,31 +1,15 @@
-/* eslint-disable quotes */
+const divElem = document.querySelector("div");
+const htmlElem = document.querySelector(":root");
 
+htmlElem.addEventListener("click", showHide);
+document.addEventListener("keydown", showHide);
 
-function listener(event) {
-  const l = document.createElement("li");
-  switch (event.type) {
-    case "animationstart":
-      l.textContent = `Started: elapsed time is ${event.elapsedTime}`;
-      break;
-    case "animationend":
-      l.textContent = `Ended: elapsed time is ${event.elapsedTime}`;
-      break;
-    case "animationiteration":
-      l.textContent = `New loop started at time ${event.elapsedTime}`;
-      break;
+function showHide() {
+  if (divElem.classList[0] === "fade-in") {
+    divElem.classList.remove("fade-in");
+    divElem.classList.add("fade-out");
+  } else {
+    divElem.classList.remove("fade-out");
+    divElem.classList.add("fade-in");
   }
-  document.getElementById("output").appendChild(l);
 }
-
-
-
-const element = document.getElementById("watchme");
-element.addEventListener("animationstart", listener, false);
-element.addEventListener("animationend", listener, false);
-element.addEventListener("animationiteration", listener, false);
-
-element.className = "slidein";
-
-
-
-
