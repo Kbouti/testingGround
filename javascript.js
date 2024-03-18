2e360fd2d1cbeb0581281ca7aae9436271777cd8
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-18 15:47:21 -0700

    Logged reading on props

--- javascript.js
+++ javascript.js
@@ -6,3 +6,6 @@ console.log(`Happy Birthday`);
 
 // Test edit
 // Performed reading on WAI-ARIA 2/23/24
+
+// Read about props and such https://react.dev/learn/passing-props-to-a-component
+// 3/18/24
