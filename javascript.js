9dabc01f60ed700036d724228d947d03302ebd6f
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-21 12:27:17 -0700

    Add fourth troublesome react challenge

--- javascript.js
+++ javascript.js
@@ -11,3 +11,61 @@ console.log(`Happy Birthday`);
 // 3/18/24
 
 // Lots and lots of React excercise work on 3/20/24
+
+
+// React exercise 4, work in progress:
+import { useState } from "react";
+import { letters } from "./data.js";
+import Letter from "./Letter.js";
+
+export default function MailClient() {
+  const [lettersArray, setLettersArray] = useState(letters);
+
+  let selectedLetters = lettersArray.filter((letter) => {
+    if (letter.isStarred === true) {
+      return letter;
+    } else return;
+  });
+
+  const selectedCount = selectedLetters.length;
+
+  function handleToggle(toggledId) {
+    let newLettersArray = lettersArray.map((letter) => {
+      if (letter.id !== toggledId) {
+        return letter;
+      } else {
+        let newLetter = {
+          id: toggledId,
+          subject: letter.subject,
+          isStarred: !letter.isStarred,
+        };
+        return newLetter;
+      }
+    });
+
+    setLettersArray(newLettersArray);
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {lettersArray.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={
+              // TODO: allow multiple selection
+              letter.isStarred
+            }
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>You selected {selectedCount} letters</b>
+        </p>
+      </ul>
+    </>
+  );
+}
