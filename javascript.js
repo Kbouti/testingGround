2f9b11d2313ee195827277ea9811d76ca03aa7e7
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-07 12:42:33 -0700

    Render dog photo with random.dog api

--- javascript.js
+++ javascript.js
@@ -1,24 +1,17 @@
+// ********************************************************************************************
+// posting a random dog picture with random.dog api:
+// https://publicapis.io/random-dog-animals-api
+// ********************************************************************************************
 
 const image = document.getElementsByClassName("giphy")[0];
 fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=5jeNgWyeCz9wEcAY4RqoWnk5ls4jayVF&s=dogs",
-  { mode: "cors" }
+  "https://random.dog/woof.json"
 ).then(function(response) {
   return response.json();
 })
 .then(function(response){
   console.log(response);
-  console.log(response.data)
-//   console.log(response.data.images.original.url);
-
-//   const URLresponse = response.data.images.original.url;
-// image.src = URLresponse;
-  // return URLresponse;
+  console.log(`response.url: ${response.url}`)
+  const URLresponse = response.url;
+image.src = URLresponse;
 })
-
-
-// This friggin worked a minute ago and now its saying response.data contains an empty array????
-// That does make it seem like it's on the server side....
-
-// IT IS A PROBLEM WITH THE SERVER!!!!!!!!
-//  Giphy says I exceeded my limit
