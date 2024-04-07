144d0bf72ff2d32332f43cf8ff72381da94f32e4
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-07 11:21:57 -0700

    Add gif from giphy with plain javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 
+const image = document.getElementsByClassName("giphy")[0];
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=5jeNgWyeCz9wEcAY4RqoWnk5ls4jayVF&s=cats",
+  { mode: "cors" }
+).then(function(response) {
+  return response.json();
+})
+.then(function(response){
+  console.log(response);
+  console.log(response.data.images.original.url);
+
+  const URLresponse = response.data.images.original.url;
+image.src = URLresponse;
+})
+
+
+
+// Success!!! We have achieved gif
