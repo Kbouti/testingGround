609fd6c2ec2913be1addb47534266b76e1c2b6ec
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-07 11:28:34 -0700

    Giphy cut us off

--- javascript.js
+++ javascript.js
@@ -1,19 +1,24 @@
 
 const image = document.getElementsByClassName("giphy")[0];
 fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=5jeNgWyeCz9wEcAY4RqoWnk5ls4jayVF&s=cats",
+  "https://api.giphy.com/v1/gifs/translate?api_key=5jeNgWyeCz9wEcAY4RqoWnk5ls4jayVF&s=dogs",
   { mode: "cors" }
 ).then(function(response) {
   return response.json();
 })
 .then(function(response){
   console.log(response);
-  console.log(response.data.images.original.url);
+  console.log(response.data)
+//   console.log(response.data.images.original.url);
 
-  const URLresponse = response.data.images.original.url;
-image.src = URLresponse;
+//   const URLresponse = response.data.images.original.url;
+// image.src = URLresponse;
+  // return URLresponse;
 })
 
 
+// This friggin worked a minute ago and now its saying response.data contains an empty array????
+// That does make it seem like it's on the server side....
 
-// Success!!! We have achieved gif
+// IT IS A PROBLEM WITH THE SERVER!!!!!!!!
+//  Giphy says I exceeded my limit
