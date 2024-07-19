9efcf2bac3db8046eb644587e892e4c0957ae47c
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-19 11:58:46 -0700

    Log SQL reading

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 // How many times has fathers day landed on June 16 since 1959
 // Fathers day is the third sunday in june
+// So ideally we just create a list of every fathers day and every bday and see which ones add up?
 
 let firstDay = new Date("1959-06-16");
 console.log(firstDay)
@@ -11,3 +12,10 @@ function addDays(date, days){
 }
 
 console.log(addDays(firstDay, 1));
+
+
+
+
+
+
+// Log SQL reading 7/19
